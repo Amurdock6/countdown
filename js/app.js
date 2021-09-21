9c0e4c09e7a6f2643d5a646c5e71199e8f64c27e
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-09-21 13:32:11 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('June 30, 2021 00:00:00').getTime()
+    const countDate = new Date('October 31, 2021 00:00:00').getTime()
 
     const now = new Date().getTime();
     const gap = countDate - now;
